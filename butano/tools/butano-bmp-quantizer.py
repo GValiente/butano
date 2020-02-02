eb7fe4790d1c10a38ebdca1d1dcaed487bd2fa34
Gustavo Valiente <gustavo.valiente@protonmail.com>
2020-02-02 17:36:42 +0100

    BMP quantizer tool

---
+++ butano/tools/butano-bmp-quantizer.py
@@ -0,0 +1,21 @@
+"""
+Copyright (c) 2020 Gustavo Valiente gustavo.valiente@protonmail.com
+MIT License, see LICENSE file.
+"""
+
+import argparse
+
+from bmp import BMP
+
+
+def process(input_file_path, output_file_path):
+    bmp = BMP(input_file_path)
+    bmp.quantize(output_file_path)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='butano BMP quantizer.')
+    parser.add_argument('--input', required=True, help='input file path')
+    parser.add_argument('--output', required=True, help='output file path')
+    args = parser.parse_args()
+    process(args.input, args.output)

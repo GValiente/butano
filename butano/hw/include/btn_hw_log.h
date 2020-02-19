5d407d40cd74bdd2af84b1063c31d2719eb36f18
Gustavo Valiente <gustavo.valiente@protonmail.com>
2020-02-19 17:47:35 +0100

    log receives an string_view (optimization)

--- butano/hw/include/btn_hw_log.h
+++ butano/hw/include/btn_hw_log.h
@@ -5,11 +5,14 @@
 #include "btn_config_log.h"
 
 #if BTN_CFG_LOG_ENABLED
-    #include "btn_string_fwd.h"
+    namespace btn
+    {
+        class string_view;
+    }
 
     namespace btn::hw
     {
-        void log(const istring& message);
+        void log(const string_view& message);
     }
 #endif
 
--- butano/hw/src/btn_hw_log.cpp
+++ butano/hw/src/btn_hw_log.cpp
@@ -1,7 +1,7 @@
 #include "../include/btn_hw_log.h"
 
 #if BTN_CFG_LOG_ENABLED
-    #include "btn_string.h"
+    #include "btn_string_view.h"
 
     #if BTN_CFG_LOG_IMPLEMENTATION == BTN_LOG_IMPLEMENTATION_NOCASHGBA
         #include "tonc.h"
@@ -13,14 +13,21 @@
     namespace btn::hw
     {
         #if BTN_CFG_LOG_IMPLEMENTATION == BTN_LOG_IMPLEMENTATION_VBA
-            void log(const istring& message)
+            void log(const string_view& message)
             {
+                string<0x100> message_string;
+
+                if(! message.empty())
+                {
+                    message_string.append(message.data(), message.size());
+                }
+
                 asm volatile
                 (
                     "mov r0, %0;"
                     "swi 0xff;"
                     :
-                    : "r" (message.c_str())
+                    : "r" (message_string.c_str())
                     : "r0"
                 );
 
@@ -34,12 +41,19 @@
                 );
             }
         #elif BTN_CFG_LOG_IMPLEMENTATION == BTN_LOG_IMPLEMENTATION_NOCASHGBA
-            void log(const istring& message)
+            void log(const string_view& message)
             {
-                nocash_puts(message.c_str());
+                string<0x100> message_string;
+
+                if(! message.empty())
+                {
+                    message_string.append(message.data(), message.size());
+                }
+
+                nocash_puts(message_string.c_str());
             }
         #elif BTN_CFG_LOG_IMPLEMENTATION == BTN_LOG_IMPLEMENTATION_MGBA
-            void log(const istring& message)
+            void log(const string_view& message)
             {
                 // https://forum.gbadev.org/viewtopic.php?f=14&p=179241&sid=aec6b23d11c25ec75966b3bbc89c91c0
 

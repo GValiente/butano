fb62afd11fb2de2465ee3ff522177f31dc889018
Gustavo Valiente <gustavo.valiente@protonmail.com>
2020-02-19 17:48:46 +0100

    log receives a string_view (optimization)

--- butano/src/btn_log.cpp
+++ butano/src/btn_log.cpp
@@ -1,21 +1,14 @@
 #include "btn_log.h"
 
 #if BTN_CFG_LOG_ENABLED
-    #include "btn_string.h"
+    #include "btn_string_view.h"
     #include "../hw/include/btn_hw_log.h"
 
     namespace btn
     {
         void log(const char* message)
         {
-            string<0x100> message_string;
-
-            if(message)
-            {
-                message_string.append(message);
-            }
-
-            hw::log(message_string);
+            hw::log(message);
         }
     }
 #endif

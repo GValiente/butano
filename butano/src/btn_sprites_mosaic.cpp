960c42d225857c462001314a76a1c00cc941b960
Gustavo Valiente <gustavo.valiente@protonmail.com>
2020-03-18 17:44:48 +0100

    mosaic joint stretch

--- butano/src/btn_sprites_mosaic.cpp
+++ butano/src/btn_sprites_mosaic.cpp
@@ -30,4 +30,19 @@ void set_vertical_stretch(fixed stretch)
     display_manager::set_sprites_mosaic_vertical_stretch(stretch);
 }
 
+void set_stretch(fixed stretch)
+{
+    BTN_ASSERT(stretch >= 0 && stretch <= 1, "Invalid stretch: ", stretch);
+
+    display_manager::set_sprites_mosaic_stretch(stretch);
+}
+
+void set_stretch(fixed horizontal_stretch, fixed vertical_stretch)
+{
+    BTN_ASSERT(horizontal_stretch >= 0 && horizontal_stretch <= 1, "Invalid horizontal stretch: ", horizontal_stretch);
+    BTN_ASSERT(vertical_stretch >= 0 && vertical_stretch <= 1, "Invalid vertical stretch: ", vertical_stretch);
+
+    display_manager::set_sprites_mosaic_stretch(horizontal_stretch, vertical_stretch);
+}
+
 }

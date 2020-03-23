65acc55ffb7fa1a232712b1c3d170c26c00096df
Gustavo Valiente <gustavo.valiente@protonmail.com>
2020-03-23 16:25:21 +0100

    bf: stage 1 enemies

--- games/butano-fighter/bgs/stage_x/tiles.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.2" name="tiles" tilewidth="16" tileheight="16" tilecount="324" columns="18">
- <image source="tileset.png" trans="000000" width="288" height="288"/>
-</tileset>

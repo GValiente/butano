afe1ab857dfe49413e967e2e4932637a9d435cdc
Gustavo Valiente <gustavo.valiente@protonmail.com>
2020-04-19 09:38:45 +0200

    bf: butano background hblank effect

--- games/butano-fighter/include/bf_butano_background.h
+++ games/butano-fighter/include/bf_butano_background.h
@@ -1,8 +1,10 @@
 #ifndef BF_BUTANO_BACKGROUND_H
 #define BF_BUTANO_BACKGROUND_H
 
+#include "btn_color.h"
 #include "btn_optional.h"
 #include "btn_regular_bg_actions.h"
+#include "btn_bg_palettes_hblank_effects.h"
 
 namespace bf
 {
@@ -13,10 +15,16 @@ class butano_background
 public:
     butano_background();
 
+    butano_background(const butano_background& other) = delete;
+
+    butano_background& operator=(const butano_background& other) = delete;
+
     void update();
 
 private:
     btn::optional<btn::regular_bg_move_by_action> _move_action;
+    btn::color _palette_hblank_effect_colors[160];
+    btn::bg_palettes_transparent_color_hblank_effect_ptr _palette_hblank_effect;
 };
 
 }
--- games/butano-fighter/src/bf_butano_background.cpp
+++ games/butano-fighter/src/bf_butano_background.cpp
@@ -1,7 +1,5 @@
 #include "bf_butano_background.h"
 
-#include "btn_colors.h"
-#include "btn_bg_palettes.h"
 #include "btn_inside_window.h"
 #include "btn_bg_items_butano_background.h"
 
@@ -10,10 +8,40 @@ namespace bf
 
 butano_background::butano_background() :
     _move_action(btn::regular_bg_move_by_action(
-                     btn::regular_bg_ptr::create(0, 0, btn::bg_items::butano_background), 1, -1))
+                     btn::regular_bg_ptr::create(0, 0, btn::bg_items::butano_background), 1, -1)),
+    _palette_hblank_effect(btn::bg_palettes_transparent_color_hblank_effect_ptr::create(_palette_hblank_effect_colors))
 {
-    btn::bg_palettes::set_transparent_color(btn::colors::maroon);
     btn::window::sprites().set_show_bg(_move_action->bg(), false);
+    btn::color color(14, 0, 0);
+    int redInc = 32;
+    int greenInc = 16;
+    int blueInc = 8;
+
+    for(int index = 0; index < 160; ++index)
+    {
+        _palette_hblank_effect_colors[index] = color;
+        --redInc;
+        --greenInc;
+        --blueInc;
+
+        if(! redInc)
+        {
+            color.set_red(color.red() + 1);
+            redInc = 32;
+        }
+
+        if(! greenInc)
+        {
+            color.set_green(color.green() + 1);
+            greenInc = 16;
+        }
+
+        if(! blueInc)
+        {
+            color.set_blue(color.blue() + 1);
+            blueInc = 16;
+        }
+    }
 }
 
 void butano_background::update()

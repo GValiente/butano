1b2be281fe9d19f403b220e95edba2a2c57fe0f5
Gustavo Valiente <gustavo.valiente@protonmail.com>
2020-07-01 17:26:47 +0200

    hdma

---
+++ butano/hw/include/btn_hw_hdma.h
@@ -0,0 +1,26 @@
+#ifndef BTN_HW_HDMA_H
+#define BTN_HW_HDMA_H
+
+#include "btn_hw_tonc.h"
+
+namespace btn::hw::hdma
+{
+
+namespace
+{
+    constexpr const int channel = 3;
+}
+
+inline void start(const uint16_t* source_ptr, int half_words, uint16_t* destination_ptr)
+{
+    DMA_TRANSFER(destination_ptr, source_ptr, half_words, channel, DMA_HDMA);
+}
+
+inline void stop()
+{
+    REG_DMA[channel].cnt = 0;
+}
+
+}
+
+#endif

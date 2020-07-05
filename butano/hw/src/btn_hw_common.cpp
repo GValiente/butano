3062c9e8a1a1128c53fa27b4fa7f5a52017591fb
Gustavo Valiente <gustavo.valiente@protonmail.com>
2020-07-05 21:15:56 +0200

    iwram usage reduced

--- butano/hw/src/btn_hw_common.cpp
+++ butano/hw/src/btn_hw_common.cpp
@@ -11,10 +11,17 @@ extern "C"
         return 0;
     }
 
-    // Disable strtol (used by tte_cmd_default) to reduce IWRAM usage:
+    // Disable strtol (used by tonc's tte_cmd_default) to reduce IWRAM usage:
     long strtol([[maybe_unused]] const char* str, char**, [[maybe_unused]] int base)
     {
         BTN_ERROR("strtol not supported: ", str, " - ", base);
         return 0;
+    }
+
+    // Disable __cxa_pure_virtual to reduce IWRAM usage:
+    // https://stackoverflow.com/questions/14689639/can-i-disable-exceptions-for-when-a-pure-virtual-function-is-called
+    void __cxa_pure_virtual()
+    {
+        BTN_ERROR("__cxa_pure_virtual not supported");
     }
 }

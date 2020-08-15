2fc3168025ba3f770ee959da44b8a51c4bcdd198
Gustavo Valiente <gustavo.valiente@protonmail.com>
2020-08-15 19:23:28 +0200

    issues: mgba 1684

---
+++ issues/mgba_1684/include/wave_generator.h
@@ -0,0 +1,83 @@
+#ifndef WAVE_GENERATOR_H
+#define WAVE_GENERATOR_H
+
+#include "btn_math.h"
+#include "btn_span.h"
+#include "btn_fixed.h"
+
+class wave_generator
+{
+
+public:
+    [[nodiscard]] constexpr int speed() const
+    {
+        return _speed;
+    }
+
+    constexpr void set_speed(int speed)
+    {
+        BTN_CONSTEXPR_ASSERT(speed >= 0, "Invalid speed");
+
+        _speed = speed;
+    }
+
+    [[nodiscard]] constexpr int amplitude() const
+    {
+        return _amplitude;
+    }
+
+    constexpr void set_amplitude(int amplitude)
+    {
+        BTN_CONSTEXPR_ASSERT(amplitude >= 1 && amplitude <= 4, "Invalid amplitude");
+
+        _amplitude = amplitude;
+    }
+
+    constexpr void generate(btn::span<btn::fixed> values) const
+    {
+        switch(_amplitude)
+        {
+
+        case 1:
+            _generate_impl<1>(_speed, values);
+            break;
+
+        case 2:
+            _generate_impl<2>(_speed, values);
+            break;
+
+        case 3:
+            _generate_impl<3>(_speed, values);
+            break;
+
+        case 4:
+            _generate_impl<4>(_speed, values);
+            break;
+
+        default:
+            BTN_CONSTEXPR_ERROR("Invalid amplitude");
+            break;
+        }
+    }
+
+private:
+    int _speed = 1024;
+    int _amplitude = 4;
+
+    template<int Amplitude>
+    constexpr static void _generate_impl(int speed, btn::span<btn::fixed>& values)
+    {
+        int a = (4096 / (1 << Amplitude));
+        int b = (1 << (Amplitude - 1));
+        btn::fixed* values_data = values.data();
+
+        for(int index = 0, limit = values.size(); index < limit; ++index)
+        {
+            int lut_angle = int(uint16_t(index * speed)) >> 7;
+            int sin = btn::lut_sin(lut_angle).data();
+            values_data[index] = (sin / a) - b;
+        }
+    }
+};
+
+#endif
---
+++ issues/mgba_1684/src/main.cpp
@@ -0,0 +1,36 @@
+#include "btn_core.h"
+#include "btn_display.h"
+#include "btn_bgs_mosaic.h"
+#include "btn_regular_bg_ptr.h"
+#include "btn_regular_bg_items_stage_1.h"
+#include "btn_regular_bg_hblank_effects.h"
+#include "wave_generator.h"
+
+namespace
+{
+    [[nodiscard]] constexpr btn::array<btn::fixed, btn::display::height()> _create_hblank_effect_deltas()
+    {
+        btn::array<btn::fixed, btn::display::height()> result;
+        wave_generator().generate(result);
+        return result;
+    }
+
+    constexpr const btn::array<btn::fixed, btn::display::height()> hblank_effect_deltas = _create_hblank_effect_deltas();
+}
+
+int main()
+{
+    btn::core::init();
+
+    btn::regular_bg_ptr bg = btn::regular_bg_items::stage_1.create_bg(0, 0);
+    bg.set_mosaic_enabled(true);
+    btn::bgs_mosaic::set_stretch(0.2);
+
+    btn::regular_bg_position_hblank_effect_ptr hblank_effect =
+            btn::regular_bg_position_hblank_effect_ptr::create_horizontal(move(bg), hblank_effect_deltas);
+
+    while(true)
+    {
+        btn::core::update();
+    }
+}

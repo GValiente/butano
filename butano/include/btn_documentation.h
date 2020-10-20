1fe394a3ad319169ae5d64b610b1ab37aefe5988
Gustavo Valiente <gustavo.valiente@protonmail.com>
2020-10-20 17:55:34 +0200

    butano: m.css documentation setup

---
+++ butano/include/btn_documentation.h
@@ -0,0 +1,38 @@
+#ifndef BTN_DOCUMENTATION_H
+#define BTN_DOCUMENTATION_H
+
+/**
+ * @namespace btn
+ * @brief Modern C++ high level GBA engine
+ */
+
+/*! \mainpage My Personal Index Page
+ *
+ * \section intro_sec Introduction
+ *
+ * This is the introduction.
+ *
+ * \section install_sec Installation
+ *
+ * \subsection step1 Step 1: Opening the box
+ *
+ * etc...
+ */
+
+/*! \page page1 A documentation page
+  \tableofcontents
+  Leading text.
+  \section sec An example section
+  This page contains the subsections \ref subsection1 and \ref subsection2.
+  For more info see page \ref page2.
+  \subsection subsection1 The first subsection
+  Text.
+  \subsection subsection2 The second subsection
+  More text.
+*/
+
+/*! \page page2 Another page
+  Even more info.
+*/
+
+#endif
---
+++ docs/conf.py
@@ -0,0 +1,9 @@
+DOXYFILE = 'Doxyfile-mcss'
+MAIN_PROJECT_URL = 'https://github.com/GValiente/butano'
+LINKS_NAVBAR1 = [
+    ("Pages", 'pages', []),
+    ("Modules", 'modules', [])
+]
+LINKS_NAVBAR2 = [
+    ("Classes", 'annotated', [])
+]

59980788f84f31fa3ae24f2ffa029ca591adb3b8
Gustavo Valiente <gustavo.valiente@protonmail.com>
2020-11-01 18:55:31 +0100

    butano: link interrupt iwram usage reduced

--- butano/hw/3rd_party/gba-link-connection/include/LinkConnection.h
+++ butano/hw/3rd_party/gba-link-connection/include/LinkConnection.h
@@ -39,7 +39,7 @@ inline void setBitHigh(u32 bit) {
     REG_SIOCNT |= 1 << bit;
 }
 
-inline u16 withHeartBit(u16 data, bool heartBit) {
+inline u32 withHeartBit(u32 data, bool heartBit) {
     return (data & ~(1 << LINK_HEART_BIT)) | (heartBit << LINK_HEART_BIT);
 }
 
@@ -50,6 +50,9 @@ struct LinkState {
     u8 _heartBits[4];
     u32 _tick = 0;
     u32 _lastIRQTick = 0;
+    volatile u16 _IRQData[4];
+    volatile u32 _IRQTick = 0;
+    volatile bool _IRQDataUpdated = false;
 
     bool isConnected() {
         return playerCount > 1;
@@ -60,11 +63,38 @@ struct LinkState {
     }
 
     bool _isOutOfSync() {
+        while(_IRQDataUpdated) {
+            _IRQDataUpdated = false;
+
+            playerCount = 0;
+            _lastIRQTick = _IRQTick;
+
+            for (u32 i = 0; i < LINK_MAX_PLAYERS; i++) {
+                u32 IRQData = _IRQData[i];
+                u32 oldHeartBit = _heartBits[i];
+                u32 newHeartBit = (IRQData & (1 << LINK_HEART_BIT)) != 0;
+                bool isConnectionAlive =
+                        IRQData != LINK_NO_DATA &&
+                        (oldHeartBit == LINK_HEART_BIT_UNKNOWN || oldHeartBit != newHeartBit);
+
+                if(isConnectionAlive) {
+                    data[i] = u16(withHeartBit(IRQData, 0));
+                    _heartBits[i] = u8(newHeartBit);
+                    playerCount++;
+                }
+                else {
+                    data[i] = LINK_NO_DATA;
+                    _heartBits[i] = LINK_HEART_BIT_UNKNOWN;
+                }
+            }
+        }
+
         return isConnected() && (_lastIRQTick != _tick || currentPlayerId >= playerCount);
     }
 
     void _sync() {
-        _lastIRQTick = _tick;
+        _IRQTick = _tick;
+        _IRQDataUpdated = true;
     }
 
     void _reset() {
@@ -76,6 +106,7 @@ struct LinkState {
         }
         _tick = 0;
         _lastIRQTick = 0;
+        _IRQDataUpdated = false;
     }
 };
 
@@ -129,43 +160,26 @@ private:
 extern LinkConnection* linkConnection;
 
 inline void ISR_serial() {
-    LinkState& oldLinkState = linkConnection->_linkState;
-    LinkState newLinkState;
+    LinkState& linkState = linkConnection->_linkState;
+    linkState._sync();
 
     if(! isBitHigh(LINK_BIT_READY) || isBitHigh(LINK_BIT_ERROR)) {
-        for (u32 i = 0; i < LINK_MAX_PLAYERS; i++) {
-            newLinkState.data[i] = LINK_NO_DATA;
-            newLinkState._heartBits[i] = LINK_HEART_BIT_UNKNOWN;
-        }
+        linkState.currentPlayerId = 0;
+
+        linkState._IRQData[0] = LINK_NO_DATA;
+        linkState._IRQData[1] = LINK_NO_DATA;
+        linkState._IRQData[2] = LINK_NO_DATA;
+        linkState._IRQData[3] = LINK_NO_DATA;
     }
     else {
-        newLinkState.currentPlayerId =
+        linkState.currentPlayerId =
                 (REG_SIOCNT & (0b11 << LINK_BITS_PLAYER_ID)) >> LINK_BITS_PLAYER_ID;
 
-        for (u32 i = 0; i < LINK_MAX_PLAYERS; i++) {
-            u32 data = REG_SIOMULTI[i];
-            u32 oldHeartBit = oldLinkState._heartBits[i];
-            u32 newHeartBit = (data & (1 << LINK_HEART_BIT)) != 0;
-            bool isConnectionAlive =
-                    data != LINK_NO_DATA &&
-                    (oldHeartBit == LINK_HEART_BIT_UNKNOWN || oldHeartBit != newHeartBit);
-
-            if(isConnectionAlive) {
-                newLinkState.data[i] = withHeartBit(data, 0);
-                newLinkState._heartBits[i] = newHeartBit;
-                newLinkState.playerCount++;
-            }
-            else {
-                newLinkState.data[i] = LINK_NO_DATA;
-                newLinkState._heartBits[i] = LINK_HEART_BIT_UNKNOWN;
-            }
-        }
+        linkState._IRQData[0] = REG_SIOMULTI[0];
+        linkState._IRQData[1] = REG_SIOMULTI[1];
+        linkState._IRQData[2] = REG_SIOMULTI[2];
+        linkState._IRQData[3] = REG_SIOMULTI[3];
     }
-
-    u32 tick = oldLinkState._tick;
-    newLinkState._tick = tick;
-    newLinkState._lastIRQTick = tick;
-    oldLinkState = newLinkState;
 }
 
 #endif  // LINK_CONNECTION_H

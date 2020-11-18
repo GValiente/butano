be17603b391848e611bb9c860cb38d0d989c7cd5
Gustavo Valiente <gustavo.valiente@protonmail.com>
2020-11-18 07:29:29 +0100

    tests: big_regular_bg_maps_tests done

--- tests/big_regular_bg_maps_tests/src/main.cpp
+++ tests/big_regular_bg_maps_tests/src/main.cpp
@@ -5,84 +5,52 @@
 
 #include "bn_core.h"
 #include "bn_keypad.h"
+#include "bn_display.h"
 #include "bn_regular_bg_ptr.h"
 #include "bn_sprite_text_generator.h"
 
 #include "info.h"
 #include "variable_8x16_sprite_font.h"
 
-#include "bn_regular_bg_items_bg_384_320.h"
-#include "bn_regular_bg_items_bg_1536_1280.h"
+#include "bn_regular_bg_items_big_map.h"
 
 namespace
 {
-    void bg_384_320_scene(bn::sprite_text_generator& text_generator)
+    void big_map_scene(bn::sprite_text_generator& text_generator)
     {
         constexpr const bn::string_view info_text_lines[] = {
+            "PAD: move BG",
+            "A: move BG faster",
+            "",
             "START: go to next scene",
         };
 
-        info info("384x320 regular BG", info_text_lines, text_generator);
+        info info("1280x768 regular BG", info_text_lines, text_generator);
 
-        bn::regular_bg_ptr bg = bn::regular_bg_items::bg_384_320.create_bg(0, 0);
-        bg.set_x(72);
-        bg.set_y(80);
-
-        while(! bn::keypad::start_pressed())
-        {
-            if(bn::keypad::left_pressed() || (bn::keypad::a_held() && bn::keypad::left_held()))
-            {
-                bg.set_x(bg.x() - 8);
-            }
-            else if(bn::keypad::right_pressed() || (bn::keypad::a_held() && bn::keypad::right_held()))
-            {
-                bg.set_x(bg.x() + 8);
-            }
-
-            if(bn::keypad::up_pressed() || (bn::keypad::a_held() && bn::keypad::up_held()))
-            {
-                bg.set_y(bg.y() - 8);
-            }
-            else if(bn::keypad::down_pressed() || (bn::keypad::a_held() && bn::keypad::down_held()))
-            {
-                bg.set_y(bg.y() + 8);
-            }
-
-            info.update();
-            bn::core::update();
-        }
-    }
-
-    void bg_1536_1280_scene(bn::sprite_text_generator& text_generator)
-    {
-        constexpr const bn::string_view info_text_lines[] = {
-            "START: go to next scene",
-        };
-
-        info info("1536x1280 regular BG", info_text_lines, text_generator);
-
-        bn::regular_bg_ptr bg = bn::regular_bg_items::bg_1536_1280.create_bg(0, 0);
-        bg.set_x(320 - 56 + 384);
-        bg.set_y(240 + 320);
+        bn::regular_bg_ptr bg = bn::regular_bg_items::big_map.create_bg(0, 0);
+        int x_limit = (bg.dimensions().width() - bn::display::width()) / 2;
+        int y_limit = (bg.dimensions().height() - bn::display::height()) / 2;
 
         while(! bn::keypad::start_pressed())
         {
-            if(bn::keypad::left_pressed() || (bn::keypad::a_held() && bn::keypad::left_held()))
+            int inc = bn::keypad::a_held() ? 8 : 1;
+
+            if(bn::keypad::left_held())
             {
-                bg.set_x(bg.x() - 8);
+                bg.set_x(bn::max(bg.x().right_shift_integer() - inc, 1 - x_limit));
             }
-            else if(bn::keypad::right_pressed() || (bn::keypad::a_held() && bn::keypad::right_held()))
+            else if(bn::keypad::right_held())
             {
-                bg.set_x(bg.x() + 8);
+                bg.set_x(bn::min(bg.x().right_shift_integer() + inc, x_limit));
             }
 
-            if(bn::keypad::up_pressed() || (bn::keypad::a_held() && bn::keypad::up_held()))
+            if(bn::keypad::up_held())
             {
-                bg.set_y(bg.y() - 8);
+                bg.set_y(bn::max(bg.y().right_shift_integer() - inc, 1 - y_limit));
             }
-            else if(bn::keypad::down_pressed() || (bn::keypad::a_held() && bn::keypad::down_held()))
+            else if(bn::keypad::down_held())
             {
-                bg.set_y(bg.y() + 8);
+                bg.set_y(bn::min(bg.y().right_shift_integer() + inc, y_limit));
             }
 
             info.update();
@@ -99,7 +67,7 @@ int main()
 
     while(true)
     {
-        bg_1536_1280_scene(text_generator);
+        big_map_scene(text_generator);
         bn::core::update();
     }
 }

8da3f93039b9dd44746689e54deb49d3eab49e23
Gustavo Valiente <gustavo.valiente@protonmail.com>
2020-12-03 06:08:50 +0100

    examples: world_map variable renamed

--- examples/world_map/src/load_attributes.bn_iwram.cpp
+++ examples/world_map/src/load_attributes.bn_iwram.cpp
@@ -7,12 +7,12 @@
 
 void load_attributes(bn::affine_bg_mat_attributes base_attributes, bn::affine_bg_mat_attributes* attributes)
 {
-    bn::fixed horizontal_scale = base_attributes.horizontal_scale();
+    bn::fixed scale = base_attributes.horizontal_scale();
 
     for(int index = 0; index < bn::display::height(); ++index)
     {
-        base_attributes.set_scale(horizontal_scale);
-        horizontal_scale += 0.003;
+        base_attributes.set_scale(scale);
+        scale += 0.003;
         attributes[index] = base_attributes;
     }
 }

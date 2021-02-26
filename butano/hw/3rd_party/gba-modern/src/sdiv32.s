2082566c0917be1effee1dc94e0d895628f9bb85
Gustavo Valiente <gustavo.valiente@protonmail.com>
2021-02-26 17:17:18 +0100

    butano: signed division fixed

--- butano/hw/3rd_party/gba-modern/src/sdiv32.s
+++ butano/hw/3rd_party/gba-modern/src/sdiv32.s
@@ -23,7 +23,6 @@ __aeabi_idivmod:
     .global __aeabi_idiv
     .type __aeabi_idiv STT_FUNC
 __aeabi_idiv:
-
     @ Test division by zero
     cmp     r1, #0
     beq     __aeabi_idiv0
@@ -31,10 +30,12 @@ __aeabi_idiv:
     @ Move the lr to r12 and make the numbers positive
     mov     r12, lr
 
+    @ bit 30 is whether the numerator is negative
     cmp     r0, #0
     rsblt   r0, #0
     orrlt   r12, #1 << 30
 
+    @ bit 31 is whether the denominator is negative
     cmp     r1, #0
     rsblt   r1, #0
     orrlt   r12, #1 << 31
@@ -43,12 +44,16 @@ __aeabi_idiv:
     .extern udiv32pastzero
     bl      udiv32pastzero
 
-    @ Test the old sign bits
-    tst     r12, #1 << 30
-    rsbne   r0, r0, #0
-    rsbne   r1, r0, #0
-    tst     r12, #1 << 31
-    rsbne   r0, r0, #0
+    @ This moves "numerator is negative" to sign flag and
+    @ "denominator is negative" to carry flag
+    movs    r2, r12, lsl #1
+
+    @ If numerator was negative (sign flag is set), negate both quotient and modulo
+    rsbmi   r0, r0, #0
+    rsbmi   r1, r1, #0
+    
+    @ If denominator was negative (carry flag is set), negate quotient
+    rsbcs   r0, r0, #0
 
     @ Erase the sign bits from the return address, and return
     bic     r12, #3 << 30
--- butano/include/bn_documentation.h
+++ butano/include/bn_documentation.h
@@ -1388,6 +1388,7 @@
  *
  * * @ref BN_VERSION_MAJOR, @ref BN_VERSION_MINOR and @ref BN_VERSION_PATCH added.
  * * bn::core::current_cpu_usage, bn::core::last_cpu_usage and bn::core::last_vblank_usage accuracy improved.
+ * * Signed division fixed.
  *
  *
  * @section changelog_6_2_0 6.2.0

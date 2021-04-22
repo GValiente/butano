3a273aca8553c2d13af93dc24cba682e3dba2ef1
Gustavo Valiente <gustavo.valiente@protonmail.com>
2021-04-22 06:20:07 +0200

    documentation: qt creator setup guide added

--- butano/include/bn_documentation.h
+++ butano/include/bn_documentation.h
@@ -1258,6 +1258,108 @@
 
 
 /**
+ * @page qt_creator Qt Creator setup guide
+ *
+ * @tableofcontents
+ *
+ * Qt Creator is a cross-platform integrated development environment (IDE) built for the maximum developer experience.
+ * Qt Creator runs on Windows, Linux, and macOS desktop operating systems,
+ * and allows developers to create applications across desktop, mobile, and embedded platforms.
+ *
+ * Since it has lots of C++ advanced features for a free IDE, it is a good fit for Butano projects.
+ *
+ * This is a small tutorial for setting up Butano's template project in Qt Creator 4.14.
+ * Although the guide has been made in Windows, the steps for other operating systems are similar if not the same.
+ *
+ *
+ * @section qt_creator_getting_started Getting started
+ *
+ * Before messing with Qt Creator, make sure that you are able to build and run Butano projects from the command line.
+ * The best way to achieve it is to read the
+ * @ref getting_started "guide to download, install and start using Butano".
+ *
+ *
+ * @section qt_creator_install Install Qt Creator
+ *
+ * If you don't have Qt Creator installed yet, you can get it from <a href="https://www.qt.io/offline-installers">here</a>.
+ *
+ * @image html qt_creator/qt_0.png
+ *
+ *
+ * @section qt_creator_compilers Compilers
+ *
+ * Once you have Qt Creator installed, add devkitARM GCC and G++ compilers from `Tools -> Options -> Kits -> Compilers`:
+ *
+ * @image html qt_creator/qt_1.png
+ *
+ * @image html qt_creator/qt_2.png
+ *
+ *
+ * @section qt_creator_kit Kit
+ *
+ * Add a new kit with the previous devkitARM compilers from `Tools -> Options -> Kits -> Kits`:
+ *
+ * @image html qt_creator/qt_3.png
+ *
+ * You can ignore the yellow triangle: it is a warning, not an error.
+ *
+ *
+ * @section qt_creator_import Import template project
+ *
+ * Now that you have the kit setup, import Butano's template project from `File -> New File or Project...`:
+ *
+ * @image html qt_creator/qt_4.png
+ *
+ * @image html qt_creator/qt_5.png
+ *
+ * @image html qt_creator/qt_6.png
+ *
+ * @image html qt_creator/qt_7.png
+ *
+ *
+ * @section qt_creator_cpp_20 C++20
+ *
+ * Enable C++20 support in the `template.cxxflags` file with the following line:
+ *
+ * @code{.cpp}
+ * -std=c++20
+ * @endcode
+ *
+ *
+ * @section qt_creator_include_paths Include paths
+ *
+ * Add the following paths to the `template.includes` file:
+ *
+ * @code{.cpp}
+ * include
+ * build
+ * ../butano/include
+ * @endcode
+ *
+ *
+ * @section qt_creator_build_settings Build settings
+ *
+ * Disable `all` build target from `Projects -> Build Settings -> Build Steps`:
+ *
+ * @image html qt_creator/qt_8.png
+ *
+ *
+ * @section qt_creator_run_settings Run settings
+ *
+ * Add your favorite GBA emulator from `Projects -> Run Settings -> Run`:
+ *
+ * @image html qt_creator/qt_9.png
+ *
+ *
+ * @section qt_creator_build_and_run Build and run
+ *
+ * Finally, build and run the template project by clicking at the bottom left arrow (the one without the roach please).
+ *
+ * If everything went as expected, your GBA emulator of choice should show a nice black screen. Congrats!
+ */
+
+
+/**
  * @page faq Frequently asked questions (FAQ)
  *
  * @tableofcontents
@@ -1277,6 +1379,11 @@
  * take a look at the <a href="modules.html">modules</a> page.
  *
  * This page is worth a look too.
+ *
+ *
+ * @subsection faq_ide Is there any good free IDE for Butano projects out there?
+ *
+ * Take a look at the @ref qt_creator.
  *
  *
  * @subsection faq_sell_money Would I be able to sell my game made with Butano for money?
@@ -1466,7 +1573,8 @@
  *
  * @section changelog_6_11_0 6.11.0 (next release)
  *
- * Affine background tiles offset optimized.
+ * * Affine background tiles offset optimized.
+ * * @ref qt_creator added.
  *
  *
  * @section changelog_6_10_0 6.10.0

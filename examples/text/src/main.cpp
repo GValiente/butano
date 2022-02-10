46055a08c44aebb748bddb8b11f141f45bd68703
laqieer <laqieer@126.com>
2022-02-11 02:48:00 +0800

    examples: common fonts contain 25000+ CJK characters
    Common fonts for:
    - Simplified Chinese
    - Traditional Chinese (Taiwan)
    - Traditional Chinese (Hong Kong)
    - Japanese
    - Korean
    
    Source Han Serif (SIL Open Font License 1.1)
    https://source.typekit.com/source-han-serif/
    https://github.com/adobe-fonts/source-han-serif

--- examples/text/src/main.cpp
+++ examples/text/src/main.cpp
@@ -255,36 +255,38 @@ namespace
         text_generator.generate(0, -text_y_limit, "BMFont text (common)", text_sprites);
         text_generator.generate(0, text_y_limit, "START: go to next scene", text_sprites);
 
+        text_generator.generate(0, -52, "CJK characters (Source Han Serif)", text_sprites);
+
         bn::sprite_text_generator common_sc_font_text_generator(common_sc_variable_16x16_sprite_font);
         common_sc_font_text_generator.set_center_alignment();
 
         bn::vector<bn::sprite_ptr, 32> common_sc_font_text_sprites;
-        common_sc_font_text_generator.generate(0, -48, "简体中文（3500个常用汉字）", common_sc_font_text_sprites);
+        common_sc_font_text_generator.generate(0, -32, "简体中文（3500个常用汉字）", common_sc_font_text_sprites);
 
         bn::sprite_text_generator common_tc_font_text_generator(common_tc_variable_16x16_sprite_font);
         common_tc_font_text_generator.set_center_alignment();
 
         bn::vector<bn::sprite_ptr, 32> common_tc_font_text_sprites;
-        common_tc_font_text_generator.generate(0, -32, "繁体中文（台灣）4808個常用國字", common_tc_font_text_sprites);
-        common_tc_font_text_generator.generate(0, -16, "+ 6341個次常用國字", common_tc_font_text_sprites);
+        common_tc_font_text_generator.generate(0, -16, "繁体中文（台灣）4808個常用國字", common_tc_font_text_sprites);
+        common_tc_font_text_generator.generate(0, 0, "+ 6341個次常用國字", common_tc_font_text_sprites);
 
         bn::sprite_text_generator common_hc_font_text_generator(common_hc_variable_16x16_sprite_font);
         common_hc_font_text_generator.set_center_alignment();
 
         bn::vector<bn::sprite_ptr, 32> common_hc_font_text_sprites;
-        common_hc_font_text_generator.generate(0, 0, "繁体中文（香港）小學學習4807字", common_hc_font_text_sprites);
+        common_hc_font_text_generator.generate(0, 16, "繁体中文（香港）小學學習4807字", common_hc_font_text_sprites);
 
         bn::sprite_text_generator common_jp_font_text_generator(common_jp_variable_16x16_sprite_font);
         common_jp_font_text_generator.set_center_alignment();
 
         bn::vector<bn::sprite_ptr, 32> common_jp_font_text_sprites;
-        common_jp_font_text_generator.generate(0, 16, "日本語「常用漢字1945字」", common_jp_font_text_sprites);
+        common_jp_font_text_generator.generate(0, 32, "日本語「常用漢字1945字」", common_jp_font_text_sprites);
 
         bn::sprite_text_generator common_kr_font_text_generator(common_kr_variable_16x16_sprite_font);
         common_kr_font_text_generator.set_center_alignment();
 
         bn::vector<bn::sprite_ptr, 32> common_kr_font_text_sprites;
-        common_kr_font_text_generator.generate(0, 32, "한국어 2350한자", common_kr_font_text_sprites);
+        common_kr_font_text_generator.generate(0, 48, "한국어 2350한자", common_kr_font_text_sprites);
 
         while(! bn::keypad::start_pressed())
         {

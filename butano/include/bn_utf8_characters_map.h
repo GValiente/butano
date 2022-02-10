3500b1bcb1138f8e99f43669745fb3cb09bde8c7
Gustavo Valiente <gustavo.valiente@protonmail.com>
2022-02-10 14:05:52 +0100

    butano: utf8_characters_map and utf8_characters_map_ref added

---
+++ butano/include/bn_utf8_characters_map.h
@@ -0,0 +1,105 @@
+/*
+ * Copyright (c) 2020-2022 Gustavo Valiente gustavo.valiente@protonmail.com
+ * zlib License, see LICENSE file.
+ */
+
+#ifndef BN_UTF8_CHARACTERS_MAP_H
+#define BN_UTF8_CHARACTERS_MAP_H
+
+/**
+ * @file
+ * bn::utf8_characters_map header file.
+ *
+ * @ingroup text
+ */
+
+#include "bn_span.h"
+#include "bn_utf8_characters_map_ref.h"
+
+namespace bn
+{
+
+/**
+ * @brief Maps a list of UTF-8 characters to their position in the list.
+ *
+ * @tparam Utf8Characters UTF-8 characters list to map.
+ *
+ * @ingroup text
+ */
+template<const span<const utf8_character>& Utf8Characters>
+class utf8_characters_map
+{
+
+public:
+    /**
+     * @brief Default constructor.
+     */
+    constexpr utf8_characters_map()
+    {
+        hash<int> hasher;
+        uint16_t character_index = 0;
+
+        for(const utf8_character& character : Utf8Characters)
+        {
+            int data = character.data();
+            auto initial_item_index = _item_index(hasher(data));
+            int item_index = initial_item_index;
+
+            while(_items[item_index].valid)
+            {
+                BN_ASSERT(data != _items[item_index].data,
+                          "There's duplicated UTF-8 characters: ", character_index);
+
+                item_index = _item_index(unsigned(item_index + 1));
+                BN_ASSERT(item_index != initial_item_index, "All items are allocated");
+            }
+
+            item_type& item = _items[item_index];
+            item.data = data;
+            item.index = character_index;
+            item.valid = true;
+            ++character_index;
+        }
+    }
+
+    /**
+     * @brief Returns a reference to this map without information about the UTF-8 characters input list.
+     *
+     * This map is not copied but referenced, so it should outlive the utf8_characters_map_ref
+     * to avoid dangling references.
+     */
+    [[nodiscard]] constexpr utf8_characters_map_ref reference() const
+    {
+        return utf8_characters_map_ref(_items[0], _items_count, Utf8Characters.size());
+    }
+
+private:
+    using item_type = utf8_characters_map_ref::item_type;
+
+    static_assert(! Utf8Characters.empty());
+
+    [[nodiscard]] constexpr static int _calculate_items_count()
+    {
+        int result = 1;
+
+        while(result < Utf8Characters.size())
+        {
+            result *= 2;
+        }
+
+        return result * 2;
+    }
+
+    static constexpr int _items_count = _calculate_items_count();
+
+    item_type _items[_items_count] = {};
+
+    [[nodiscard]] constexpr static int _item_index(unsigned hash)
+    {
+        return int(hash & (_items_count - 1));
+    }
+};
+
+}
+
+#endif
---
+++ butano/include/bn_utf8_characters_map_ref.h
@@ -0,0 +1,123 @@
+/*
+ * Copyright (c) 2020-2022 Gustavo Valiente gustavo.valiente@protonmail.com
+ * zlib License, see LICENSE file.
+ */
+
+#ifndef BN_UTF8_CHARACTERS_MAP_REF_H
+#define BN_UTF8_CHARACTERS_MAP_REF_H
+
+/**
+ * @file
+ * bn::utf8_characters_map header file.
+ *
+ * @ingroup text
+ */
+
+#include "bn_power_of_two.h"
+#include "bn_utf8_character.h"
+
+namespace bn
+{
+
+/**
+ * @brief Maps a list of UTF-8 characters to their position in the list.
+ *
+ * @ingroup text
+ */
+class utf8_characters_map_ref
+{
+
+public:
+    /**
+     * @brief Map item info.
+     */
+    struct item_type
+    {
+        int data = 0; //!< UTF-8 character data.
+        unsigned index: 31 = 0; //!< Position of the UTF-8 character in the input list.
+        bool valid: 1 = false; //!< Indicates if the item is valid or not.
+    };
+
+    /**
+     * @brief Default class constructor.
+     */
+    constexpr utf8_characters_map_ref() = default;
+
+    /**
+     * @brief Class constructor.
+     * @param items_ref Reference to a list of map items.
+     *
+     * Map items are not copied but referenced, so they should outlive the utf8_characters_map_ref
+     * to avoid dangling references.
+     *
+     * @param items_count Referenced map items count.
+     * @param characters_count Mapped UTF-8 characters count.
+     */
+    constexpr utf8_characters_map_ref(const item_type& items_ref, int items_count, int characters_count) :
+        _items(&items_ref),
+        _items_count_minus_one(items_count - 1),
+        _characters_count(characters_count)
+    {
+        BN_ASSERT(power_of_two(items_count), "Items count must be a power of two: ", items_count);
+        BN_ASSERT(characters_count > 0 && characters_count <= items_count,
+                  "Invalid characters count: ", characters_count, " - ", items_count);
+    }
+
+    /**
+     * @brief Returns the mapped UTF-8 characters count.
+     */
+    [[nodiscard]] constexpr int size() const
+    {
+        return _characters_count;
+    }
+
+    /**
+     * @brief Indicates if it doesn't contain any mapped UTF-8 character.
+     */
+    [[nodiscard]] constexpr bool empty() const
+    {
+        return _characters_count == 0;
+    }
+
+    /**
+     * @brief Searches for the position of a given UTF-8 character in the input list.
+     * @param character UTF-8 character to search for.
+     * @return Position of the UTF-8 character in the input list.
+     */
+    [[nodiscard]] constexpr int index(const utf8_character& character) const
+    {
+        hash<int> hasher;
+        int key = character.data();
+        int index = _item_index(hasher(key));
+        int its = 0;
+        int items_count = _items_count_minus_one + 1;
+
+        while(its < items_count && _items[index].valid)
+        {
+            if(key == _items[index].data)
+            {
+                return _items[index].index;
+            }
+
+            index = _item_index(unsigned(index + 1));
+            ++its;
+        }
+
+        BN_ERROR("UTF-8 character not found");
+        return 0;
+    }
+
+private:
+    const item_type* _items = nullptr;
+    int _items_count_minus_one = -1;
+    int _characters_count = 0;
+
+    [[nodiscard]] constexpr int _item_index(unsigned hash) const
+    {
+        return int(hash & unsigned(_items_count_minus_one));
+    }
+};
+
+}
+
+#endif

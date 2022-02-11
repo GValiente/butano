cc700063241187c66b7ed8aadd02befcdf006b8d
laqieer <laqieer@126.com>
2022-02-11 19:29:54 +0800

    Seperate common fonts to its own repo: https://github.com/laqieer/gba-free-fonts
    - to avoid increasing butano's repo size
    - independent maintain and update from butano main engine
    - to support more game engines or GBA projects besides butano
    
    https://github.com/laqieer/gba-free-fonts/commit/12e34163c1b87adf2cc4f6c16829a82c8d78d62d.

--- examples/text/src/main.cpp
+++ examples/text/src/main.cpp
@@ -24,11 +24,6 @@
 #include "common_fixed_8x16_sprite_font.h"
 #include "common_variable_8x8_sprite_font.h"
 #include "common_variable_8x16_sprite_font.h"
-#include "common_sc_variable_16x16_sprite_font.h"
-#include "common_tc_variable_16x16_sprite_font.h"
-#include "common_hc_variable_16x16_sprite_font.h"
-#include "common_jp_variable_16x16_sprite_font.h"
-#include "common_kr_variable_16x16_sprite_font.h"
 
 namespace
 {
@@ -245,54 +240,6 @@ namespace
             bn::core::update();
         }
     }
-
-    void common_font_text_scene()
-    {
-        bn::sprite_text_generator text_generator(common::variable_8x16_sprite_font);
-        text_generator.set_center_alignment();
-
-        bn::vector<bn::sprite_ptr, 32> text_sprites;
-        text_generator.generate(0, -text_y_limit, "BMFont text (common)", text_sprites);
-        text_generator.generate(0, text_y_limit, "START: go to next scene", text_sprites);
-
-        text_generator.generate(0, -52, "CJK characters (Source Han Serif)", text_sprites);
-
-        bn::sprite_text_generator common_sc_font_text_generator(common_sc_variable_16x16_sprite_font);
-        common_sc_font_text_generator.set_center_alignment();
-
-        bn::vector<bn::sprite_ptr, 32> common_sc_font_text_sprites;
-        common_sc_font_text_generator.generate(0, -32, "简体中文（3500个常用汉字）", common_sc_font_text_sprites);
-
-        bn::sprite_text_generator common_tc_font_text_generator(common_tc_variable_16x16_sprite_font);
-        common_tc_font_text_generator.set_center_alignment();
-
-        bn::vector<bn::sprite_ptr, 32> common_tc_font_text_sprites;
-        common_tc_font_text_generator.generate(0, -16, "繁体中文（台灣）4808個常用國字", common_tc_font_text_sprites);
-        common_tc_font_text_generator.generate(0, 0, "+ 6341個次常用國字", common_tc_font_text_sprites);
-
-        bn::sprite_text_generator common_hc_font_text_generator(common_hc_variable_16x16_sprite_font);
-        common_hc_font_text_generator.set_center_alignment();
-
-        bn::vector<bn::sprite_ptr, 32> common_hc_font_text_sprites;
-        common_hc_font_text_generator.generate(0, 16, "繁体中文（香港）小學學習4807字", common_hc_font_text_sprites);
-
-        bn::sprite_text_generator common_jp_font_text_generator(common_jp_variable_16x16_sprite_font);
-        common_jp_font_text_generator.set_center_alignment();
-
-        bn::vector<bn::sprite_ptr, 32> common_jp_font_text_sprites;
-        common_jp_font_text_generator.generate(0, 32, "日本語「常用漢字1945字」", common_jp_font_text_sprites);
-
-        bn::sprite_text_generator common_kr_font_text_generator(common_kr_variable_16x16_sprite_font);
-        common_kr_font_text_generator.set_center_alignment();
-
-        bn::vector<bn::sprite_ptr, 32> common_kr_font_text_sprites;
-        common_kr_font_text_generator.generate(0, 48, "한국어 2350한자", common_kr_font_text_sprites);
-
-        while(! bn::keypad::start_pressed())
-        {
-            bn::core::update();
-        }
-    }
 }
 
 int main()
@@ -325,9 +272,6 @@ int main()
         bn::core::update();
 
         user_font_text_scene();
-        bn::core::update();
-
-        common_font_text_scene();
         bn::core::update();
     }
 }

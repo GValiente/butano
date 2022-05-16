0025d1fa3c3b9e5778ef08188a3803ecfa639b87
Gustavo Valiente <gustavo.valiente@protonmail.com>
2022-05-16 09:35:11 +0200

    tools: remove pkg_resources usage in s3m.py

--- butano/tools/s3m2gbt/s3m.py
+++ butano/tools/s3m2gbt/s3m.py
@@ -1,13 +1,9 @@
 # This is a generated file! Please edit source .ksy file and use kaitai-struct-compiler to rebuild
 
-from pkg_resources import parse_version
 from s3m2gbt import kaitaistruct
 from s3m2gbt.kaitaistruct import KaitaiStruct, KaitaiStream, BytesIO
 from enum import Enum
 
-
-if parse_version(kaitaistruct.__version__) < parse_version('0.9'):
-    raise Exception("Incompatible Kaitai Struct Python API: 0.9 or later is required, but you have %s" % (kaitaistruct.__version__))
 
 class S3m(KaitaiStruct):
     """Scream Tracker 3 module is a tracker music file format that, as all

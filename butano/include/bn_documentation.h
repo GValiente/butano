60cf1be461fe903034b7ab276be27d51be097734
GValiente <gustavo.valiente@protonmail.com>
2023-02-01 08:15:22 +0100

    documentation: faq page improved

--- butano/include/bn_documentation.h
+++ butano/include/bn_documentation.h
@@ -1986,20 +1986,20 @@
  * You can use <a href="https://openmpt.org/">OpenMPT</a> to do that.
  *
  *
- * @subsection faq_audio_quality How can I improve audio quality?
+ * @subsection faq_audio_quality How can I improve Direct Sound audio quality?
  *
- * If you have some free CPU left, you can increase audio mixing rate to improve its quality.
+ * If you have some free CPU left, you can increase Direct Sound audio mixing rate to improve its quality.
  *
- * The easiest way to specify the audio mixing rate for a specific project is
+ * The easiest way to specify Direct Sound audio mixing rate for a specific project is
  * to define it in the `USERFLAGS` of its `Makefile`.
  *
- * For example, to set the audio mixing rate to 21KHz:
+ * For example, to set Direct Sound audio mixing rate to 21KHz:
  *
  * `USERFLAGS := -DBN_CFG_AUDIO_MIXING_RATE=BN_AUDIO_MIXING_RATE_21_KHZ`
  *
  * Remember to rebuild your project from scratch after modifying a `Makefile`.
  *
- * Available mixing rates are @ref audio "here".
+ * Available Direct Sound mixing rates are @ref audio "here".
  *
  *
  * @section faq_flash_carts Flash carts

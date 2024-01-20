1d5fc9b79487df4635b3e7dc6cf0665fee6ef21a
GValiente <gustavo.valiente@protonmail.com>
2024-01-20 07:23:23 +0100

    butano: small rom usage reduction

--- butano/src/bn_translation_unit.cpp
+++ butano/src/bn_translation_unit.cpp
@@ -3,12 +3,14 @@
  * zlib License, see LICENSE file.
  */
 
+#include "bn_color_effect.cpp.h"
+#include "bn_generic_pool.cpp.h"
+#include "bn_timer.cpp.h"
+
 #include "bn_backdrop.cpp.h"
 #include "bn_format.cpp.h"
-#include "bn_generic_pool.cpp.h"
 #include "bn_log.cpp.h"
 #include "bn_math.cpp.h"
 #include "bn_reciprocal_lut.cpp.h"
 #include "bn_sin_lut.cpp.h"
 #include "bn_sram.cpp.h"
-#include "bn_timer.cpp.h"

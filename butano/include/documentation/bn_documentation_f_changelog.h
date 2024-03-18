f50cbb2d211831dae0e82155616737d98680b559
GValiente <gustavo.valiente@protonmail.com>
2024-03-18 09:44:08 +0100

    17.3.0

--- butano/include/documentation/bn_documentation_f_changelog.h
+++ butano/include/documentation/bn_documentation_f_changelog.h
@@ -12,7 +12,7 @@
  * @tableofcontents
  *
  *
- * @section changelog_17_3_0 17.3.0 (next release)
+ * @section changelog_17_3_0 17.3.0
  *
  * * bn::core::current_cpu_ticks, bn::core::last_cpu_ticks and bn::core::last_vblank_ticks added.
  * * Import tool now allows to exclude Huffman when choosing the best compression for images.

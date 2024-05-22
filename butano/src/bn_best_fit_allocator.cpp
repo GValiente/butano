e9227e2527d9df12563bd1ffd058458b3f5444fd
GValiente <gustavo.valiente@protonmail.com>
2024-05-22 19:11:27 +0200

    butano: copyright updated a bit too late

--- butano/src/bn_best_fit_allocator.cpp
+++ butano/src/bn_best_fit_allocator.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020-2023 Gustavo Valiente gustavo.valiente@protonmail.com
+ * Copyright (c) 2020-2024 Gustavo Valiente gustavo.valiente@protonmail.com
  * zlib License, see LICENSE file.
  */
 

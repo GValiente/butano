c66094ae514c74068f992a4896c5e1f234e9f6e2
Gustavo Valiente <gustavo.valiente@protonmail.com>
2026-08-03 10:10:18 +0200

    21.8.0

--- butano/include/documentation/bn_documentation_g_changelog.h
+++ butano/include/documentation/bn_documentation_g_changelog.h
@@ -12,7 +12,7 @@
  * @tableofcontents
  *
  *
- * @section changelog_21_8_0 21.8.0 (next release)
+ * @section changelog_21_8_0 21.8.0
  *
  * * bn::sprite_animate_action::set_current_index and bn::sprite_cached_animate_action::set_current_index
  *   allow to update their sprite on the next tiles change.
